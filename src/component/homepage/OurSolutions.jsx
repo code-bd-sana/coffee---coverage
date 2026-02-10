c891751a84f8116cfa604be3258f4e322f5c1d2c
rakib3719 <rakib3719>
2026-02-10 11:28:21 +0600

    solution title integrate

--- src/component/homepage/OurSolutions.jsx
+++ src/component/homepage/OurSolutions.jsx
@@ -5,9 +5,13 @@ const OurSolutions = () => {
     <div>
       {/* heading */}
 
-      <section>
-        <h1>{solution.title}</h1>
-        <p>{solution.subtitle}</p>
+      <section className='text-center mt-8'>
+        <h1 className='title-primary py-2'>{solution.title}</h1>
+        <p className='color-primary max-w-200 mx-auto text-primary'>
+          {solution.subtitle}
+        </p>
+
+        {/* solutio */}
       </section>
     </div>
   );
